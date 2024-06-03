06b9cabe4b36f1f55a29a3a3bae919b7da01b618
anupamababu1984 <158604256+anupamababu1984@users.noreply.github.com>
2024-06-03 20:52:20 +0530

    Update h5_to_tensorflow.py

--- h5_to_tensorflow.py
+++ h5_to_tensorflow.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
 
-model = tf.keras.models.load_model('trained_keras_model.h5')
+model = tf.keras.models.load_model('my_model_new.h5')
 tf.saved_model.save(model, '/tmp/my_saved_model')
