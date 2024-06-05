b07eedb1ac5e1dc1a58aff556bb7f1c62d233ae6
anupamababu1984 <158604256+anupamababu1984@users.noreply.github.com>
2024-06-05 03:53:31 -0500

    Create convert_to_tarfile.py

---
+++ convert_to_tarfile.py
@@ -0,0 +1,32 @@
+import os
+import tarfile
+import shutil
+
+# Path to the directory containing the .pb model file
+pb_model_dir = 'C:\\Users\\awsli\\kserve-tensorflow\\saved_model'
+
+# Name of the .pb model file
+pb_model_file = 'cmodel.pb'
+
+# Output tar.gz file path
+output_tar_gz_file = 'C:\\Users\\awsli\\kserve-tensorflow\\saved_model\\saved_model.tar.gz'
+
+try:
+    # Create a temporary directory to hold the .pb model file
+    temp_dir = '/c/Users/awsli/kserve-tensorflow/temp'
+    os.makedirs(temp_dir, exist_ok=True)
+    
+    # Copy the .pb model file to the temporary directory
+    shutil.copy(os.path.join(pb_model_dir, pb_model_file), temp_dir)
+    
+    # Create a tar.gz file
+    with tarfile.open(output_tar_gz_file, 'w:gz') as tar:
+        # Add the contents of the temporary directory to the tar file
+        tar.add(temp_dir, arcname=os.path.basename(pb_model_dir))
+    
+    print(f"Successfully created tar.gz file: {output_tar_gz_file}")
+except Exception as e:
+    print(f"An error occurred while creating the tar.gz file: {e}")
+finally:
+    # Clean up: Remove the temporary directory
+    shutil.rmtree(temp_dir, ignore_errors=True)
