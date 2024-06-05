1cf4108cb74bf2093e61d46467b8f67941cc27a4
anupamababu1984 <158604256+anupamababu1984@users.noreply.github.com>
2024-06-05 03:39:41 -0500

    Update h5_to_tensorflow.py

--- h5_to_tensorflow.py
+++ h5_to_tensorflow.py
@@ -7,8 +7,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # Load the Keras model
 model = tf.keras.models.load_model('my_model.h5')
 
-# Save the model in the TensorFlow SavedModel format
-tf.saved_model.save(model, '/c/Users/awsli/kserve-tensorflow/saved_model')
+# Define the directory to save the model
+save_dir = '/c/Users/awsli/kserve-tensorflow/saved_model'
 
-print("Successfully saved the model as a TensorFlow SavedModel.")
+try:
+    # Save the model in the TensorFlow SavedModel format
+    tf.saved_model.save(model, save_dir)
+    print("Successfully saved the model as a TensorFlow SavedModel.")
+except Exception as e:
+    print("An error occurred while saving the model:", e)
+
 
