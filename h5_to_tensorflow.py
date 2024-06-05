114f1b1d131636b4ccad2d976e326399e38d4fd9
anupamababu1984 <158604256+anupamababu1984@users.noreply.github.com>
2024-06-05 03:34:16 -0500

    Update h5_to_tensorflow.py

--- h5_to_tensorflow.py
+++ h5_to_tensorflow.py
@@ -4,3 +4,4 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 model = tf.keras.models.load_model('my_model.h5')
 tf.saved_model.save(model, '/c/Users/awsli/kserve-tensorflow/cmodel.pb')
+print("successfully saved")
