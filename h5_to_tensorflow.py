8b0ddfbfaedcab7d5990962e3fb35b62d162bec8
anupamababu1984 <158604256+anupamababu1984@users.noreply.github.com>
2024-06-05 03:37:38 -0500

    Update h5_to_tensorflow.py

--- h5_to_tensorflow.py
+++ h5_to_tensorflow.py
@@ -1,7 +1,14 @@
 import tensorflow as tf
 import os
+
+# Suppress TensorFlow logging messages
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+# Load the Keras model
 model = tf.keras.models.load_model('my_model.h5')
-tf.saved_model.save(model, '/c/Users/awsli/kserve-tensorflow/cmodel.pb')
-print("successfully saved")
+
+# Save the model in the TensorFlow SavedModel format
+tf.saved_model.save(model, '/c/Users/awsli/kserve-tensorflow/saved_model')
+
+print("Successfully saved the model as a TensorFlow SavedModel.")
+
