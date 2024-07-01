b203d63fe3c439390e4e2b3d69f94bc77ec80933
anupamababu1984 <158604256+anupamababu1984@users.noreply.github.com>
2024-07-01 12:21:08 -0500

    Update convert_pb_to_zip.py

--- convert_pb_to_zip.py
+++ convert_pb_to_zip.py
@@ -13,6 +13,6 @@ def convert_pb_to_zip(pb_file_path, zip_file_path):
 
 # Replace 'yourfile.pb' and 'yourfile.zip' with your file paths
 pb_file_path = 'C:\\Users\\awsli\\kserve-tensorflow\\saved_model\\0001'
-zip_file_path = 'C:\\Users\\awsli\\kserve-tensorflow\\saved_model\\saved_model.zip'
+zip_file_path = 'C:\\Users\\awsli\\kserve-tensorflow\\saved_model'
 
 convert_pb_to_zip(pb_file_path, zip_file_path)
